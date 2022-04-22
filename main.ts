214e53e3e94e2221654279729a15d50d4c03ccef
aomona <104058728+aomona@users.noreply.github.com>
2022-04-22 15:12:42 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,4 +13,16 @@ basic.forever(function () {
     } else {
         x = 2
     }
+    if (input.rotation(Rotation.Pitch) < -30) {
+        y = 0
+    } else if (input.rotation(Rotation.Pitch) < -20) {
+        y = 1
+    } else if (input.rotation(Rotation.Pitch) < 30) {
+        y = 4
+    } else if (input.rotation(Rotation.Pitch) < 20) {
+        y = 3
+    } else {
+        y = 2
+    }
+    led.plot(x, y)
 })
